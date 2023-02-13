c5abf461904e2f69c6efb09c1a23712c22accf39
shane-sev <70693591+shane-sev@users.noreply.github.com>
2023-02-13 02:53:28 -1000

    initial setup (directories and client side)

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// boiler plate for running application
+const App = () => {
+    return (
+        <div>
+            <h1>JustChat Application</h1>
+        </div>
+    );
+}
+
+export default App;
