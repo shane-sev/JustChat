a1db429ebc6feaf6fcf4cdf821767bde857c0dc5
shane-sev <70693591+shane-sev@users.noreply.github.com>
2023-02-13 22:42:50 -1000

    restarting setup

--- client/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import App from './App';
-
-// renders entire React application, linked to root div
-ReactDOM.render(<App />, document.getElementById('root'));
